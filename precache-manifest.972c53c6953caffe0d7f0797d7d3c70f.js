4a17b3998d4831fe7bc17f51e3903c221fa046e1
Veronica Racca <veronica.racca@gmail.com>
2020-07-22 22:41:41 +0200

    Updates

---
+++ precache-manifest.972c53c6953caffe0d7f0797d7d3c70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69609c8ed07156592b6b4ea2bc08a2a0",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "378429a18ebe9a21aead",
+    "url": "/pokedex/static/css/main.9a59877e.chunk.css"
+  },
+  {
+    "revision": "e1ebd9d4850a13130c12",
+    "url": "/pokedex/static/js/2.7aa4c2be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.7aa4c2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378429a18ebe9a21aead",
+    "url": "/pokedex/static/js/main.bdf895e1.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
