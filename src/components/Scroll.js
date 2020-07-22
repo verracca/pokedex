1dc43fdf5fac0639ac59a1fafd37ecc4ab501b8e
Veronica Racca <veronica.racca@gmail.com>
2020-07-23 00:07:04 +0200

    Fix css for small devices

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import "./../App.css";
 
 function Scroll(props) {
   return (
-    <div>
+    <div className="scrollContainer">
       <div className="scrollable">{props.children}</div>
     </div>
   );
